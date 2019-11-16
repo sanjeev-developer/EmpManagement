2e85b91a006ccce1254a3728adbaecc972792524
Vivek Madishetty <madishetty.vivek@gmail.com>
2019-11-16 18:52:25 -0500

    tableview commit

--- EmpManagement/Components/CoustmerlistViewController.swift
+++ EmpManagement/Components/CoustmerlistViewController.swift
@@ -11,3 +11,12 @@ var optionSelect : String!
 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return( UserDetails.shared.userarray.count)
 }
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tabelview.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CoustmerTableViewCell
+    cell.txtCellName.text =  UserDetails.shared.userarray[indexPath.row].empname
+    cell.txtCellEmail.text =  UserDetails.shared.userarray[indexPath.row].empid
+    cell.txtCellPhone.text =  UserDetails.shared.userarray[indexPath.row].dateofbirth
+    return cell
+}

455eb586d05e0d0ad37f8f192e44f8463b33dcd5
Sanjeev <sanjeevgupta9223@gmail.com>
2019-11-16 15:40:45 -0500

    commit

--- EmpManagement/Components/AddBillViewController.swift
+++ EmpManagement/Components/AddBillViewController.swift
@@ -41,6 +41,7 @@ class AddBillViewController: UIViewController  {
     var veharray : [Vehicledata] = []
     @IBOutlet var but_saveemp: UIButton!
     
+    var seglect : Int = 0
     
     
     @IBAction func addemployee(_ sender: Any) {

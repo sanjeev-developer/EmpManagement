200afdd0e340c05a3101d3c71de7169f4a6aa250
Sanjeev <sanjeevgupta9223@gmail.com>
2019-11-16 15:13:49 -0500

    Sanjeev commit 2

---
+++ EmpManagement/Components/WebviewViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WebviewViewController.swift
+//  EmpManagement
+//
+//  Created by sanjeev gupta on 2019-11-16.
+//  Copyright © 2019 sanjeev gupta. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebviewViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet var webflow: WKWebView!
+    var optionSelect : String = ""
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if(optionSelect == "about")
+        {
+            let url = URL(string: "https://www.hackingwithswift.com")!
+            webflow.load(URLRequest(url: url))
+            webflow.allowsBackForwardNavigationGestures = true
+        }
+        else if(optionSelect == "contact")
+        {
+            let url = URL(string: "https://www.hackingwithswift.com")!
+            webflow.load(URLRequest(url: url))
+            webflow.allowsBackForwardNavigationGestures = true
+        }
+       
+        
+    }
+    
+   override func loadView() {
+        webflow = WKWebView()
+        webflow.navigationDelegate = self
+        view = webflow
+    }
+    
+
+
+}

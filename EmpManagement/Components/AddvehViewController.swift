93d226e2f165c21415d862b84adbf5370ac40231
Ritik Jagpal <Ritjagpal26>
2019-11-16 21:18:34 -0500

    commit

--- EmpManagement/Components/AddvehViewController.swift
+++ EmpManagement/Components/AddvehViewController.swift
@@ -22,7 +22,13 @@ class AddvehViewController: UIViewController {
                    var vehtype : String = ""
                    var pickerView = ToolbarPickerView()
     
-   
+    @IBOutlet var testimage: UIImageView!
+    
+    
+                   @IBOutlet var vehsegment: UISegmentedControl!
+    
+                   @IBOutlet var but_addveh: UIButton!
+    
     
     @IBAction func actionvehsegment(_ sender: Any) {
         

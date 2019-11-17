459c2d95210c5031afaa7bcf0d3b0a761d4a43bb
Vivek Madishetty <madishetty.vivek@gmail.com>
2019-11-16 19:33:52 -0500

    commit

--- EmpManagement/Components/Choosepicker.swift
+++ EmpManagement/Components/Choosepicker.swift
@@ -40,3 +40,17 @@ let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, act
 let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
 let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(self.cancelTapped))
 
+toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+toolBar.isUserInteractionEnabled = true
+     
+    self.toolbar = toolBar
+    }
+
+    @objc func doneTapped() {
+        self.toolbarDelegate?.didTapDone()
+    }
+
+    @objc func cancelTapped() {
+        self.toolbarDelegate?.didTapCancel()
+    }
+}

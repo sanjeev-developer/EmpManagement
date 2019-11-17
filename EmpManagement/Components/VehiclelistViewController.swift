dd7651427eee7f63294d784ac80d81ddb4b5f6d4
Ritik Jagpal <Ritjagpal26>
2019-11-16 21:16:14 -0500

    commit

--- EmpManagement/Components/VehiclelistViewController.swift
+++ EmpManagement/Components/VehiclelistViewController.swift
@@ -10,72 +10,70 @@ import UIKit
 
 class VehiclelistViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
 
-@IBOutlet var vehicletabel: UITableView!
-@IBOutlet var but_back_vlist: UIButton!
-@IBOutlet var but_add_vehicle: UIButton!
-var position : Int!
-
-
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return( UserDetails.shared.userarray[position].Vehicle.count)
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-let cell = vehicletabel.dequeueReusableCell(withIdentifier: "vehiclecell", for: indexPath) as! VehicleListTableViewCell
-cell.txt_vehicle_type.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].vehicletype
-cell.txt_vehicle_model.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].model
-cell.txt_vehicle_plate.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].plate
-cell.txt_comapny.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].company
-
-if(UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Honda")
-{
- cell.img_list.image = UIImage(named: "honda")
-}
-else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Chervolet")
-{
-    cell.img_list.image = UIImage(named: "cheve")
-}
-else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "BMW")
-{
-    cell.img_list.image = UIImage(named: "bmw")
-}
-else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Mercedes")
-{
-    cell.img_list.image = UIImage(named: "mercedes")
-}
-else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Landrover")
-{
-    cell.img_list.image = UIImage(named: "landrover")
-}
-else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Bajaj")
-{
-    cell.img_list.image = UIImage(named: "chetak")
-}
-else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Ford")
-                  {
-                      cell.img_list.image = UIImage(named: "ford")
-                  }
-                  else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Suzuki")
-                  {
-                      cell.img_list.image = UIImage(named: "suzuki")
-                  }
-else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Audi")
-                                  {
-                                      cell.img_list.image = UIImage(named: "Audi")
-                                  }
-               
     
-    
-    
-    return cell
-}
+    @IBOutlet var vehicletabel: UITableView!
+    @IBOutlet var but_back_vlist: UIButton!
+    @IBOutlet var but_add_vehicle: UIButton!
+    var position : Int!
 
-func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 100
-}
-
-func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return( UserDetails.shared.userarray[position].Vehicle.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = vehicletabel.dequeueReusableCell(withIdentifier: "vehiclecell", for: indexPath) as! VehicleListTableViewCell
+        cell.txt_vehicle_type.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].vehicletype
+        cell.txt_vehicle_model.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].model
+        cell.txt_vehicle_plate.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].plate
+        cell.txt_comapny.text =  UserDetails.shared.userarray[position].Vehicle[indexPath.row].company
+        
+        if(UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Honda")
+                   {
+                    cell.img_list.image = UIImage(named: "honda")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Chervolet")
+                   {
+                       cell.img_list.image = UIImage(named: "cheve")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "BMW")
+                   {
+                       cell.img_list.image = UIImage(named: "bmw")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Mercedes")
+                   {
+                       cell.img_list.image = UIImage(named: "mercedes")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Landrover")
+                   {
+                       cell.img_list.image = UIImage(named: "landrover")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Bajaj")
+                   {
+                       cell.img_list.image = UIImage(named: "chetak")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Ford")
+                   {
+                       cell.img_list.image = UIImage(named: "ford")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Suzuki")
+                   {
+                       cell.img_list.image = UIImage(named: "suzuki")
+                   }
+                   else if (UserDetails.shared.userarray[position].Vehicle[indexPath.row].company == "Audi")
+                                      {
+                                          cell.img_list.image = UIImage(named: "Audi")
+                                      }
+                   
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         print(indexPath.row)
         
@@ -97,28 +95,28 @@ func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 // Do any additional setup after loading the view.
     }
 
-@IBAction func but_back_clist(_ sender: Any) {
+    @IBAction func but_back_clist(_ sender: Any) {
+        
+        let obj = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        self.navigationController?.pushViewController(obj, animated: true)
+    }
     
-    let obj = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
-    self.navigationController?.pushViewController(obj, animated: true)
-}
-
-func deletevehicle(data : Int)
-{
-    UserDetails.shared.userarray[position].Vehicle.remove(at: data)
-    DispatchQueue.main.async { self.vehicletabel.reloadData() }
-}
-
-func movetoedit(data : Int)
-{
-    let obj = self.storyboard?.instantiateViewController(withIdentifier: "AddvehViewController") as! AddvehViewController
-  obj.vehicleposition = data
-  obj.userposition = position
-  obj.purpose = "edit"
-    self.navigationController?.pushViewController(obj, animated: true)
-}
-
-@IBAction func butmovetoadd(_ sender: Any) {
+    func deletevehicle(data : Int)
+    {
+        UserDetails.shared.userarray[position].Vehicle.remove(at: data)
+        DispatchQueue.main.async { self.vehicletabel.reloadData() }
+    }
+    
+    func movetoedit(data : Int)
+      {
+          let obj = self.storyboard?.instantiateViewController(withIdentifier: "AddvehViewController") as! AddvehViewController
+        obj.vehicleposition = data
+        obj.userposition = position
+        obj.purpose = "edit"
+          self.navigationController?.pushViewController(obj, animated: true)
+      }
+    
+    @IBAction func butmovetoadd(_ sender: Any) {
         
         let obj = self.storyboard?.instantiateViewController(withIdentifier: "AddvehViewController") as! AddvehViewController
         obj.userposition = position
